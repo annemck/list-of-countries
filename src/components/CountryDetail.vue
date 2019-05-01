2c535c08a4f9dfe4028940d15f42b8c516b614a9
annemck <annekh88@gmail.com>
2019-05-01 16:19:14 +0100

    believed to have completed MVP

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div v-if="country">
+    <p>{{country.name}}</p>
+    <p>{{country.capital}}</p>
+    <p>{{country.population}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/ListItems.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <li v-on:click="handleClick">{{country.name}}</li>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../main.js';
+
+export default {
+  name: 'list-item',
+  props: ['country'],
+  methods: {
+    handleClick: function(){
+      eventBus.$emit('selected-country', this.country);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
